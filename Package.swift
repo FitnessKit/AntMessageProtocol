d4f026d44a217d826c4baf64b260f259b903c591
Kevin A. Hoogheem <kevin@hoogheem.net>
2019-04-14 12:03:45 -0500

    package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "AntMessageProtocol",
+    //platforms: [.iOS("10.0"), .macOS("10.12"), .tvOS("10.0"), .watchOS("3.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,8 +14,8 @@ let package = Package(
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/FitnessKit/FitnessUnits", from: "2.1.3"),
-        .package(url: "https://github.com/FitnessKit/DataDecoder", from: "4.3.1"),
+        .package(url: "https://github.com/FitnessKit/FitnessUnits", from: "2.1.4"),
+        .package(url: "https://github.com/FitnessKit/DataDecoder", from: "4.4.0"),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- test.dockerfile
+++
@@ -1,4 +0,0 @@
-FROM swift:4.2
-COPY . /ANTMessageProtocol
-WORKDIR /ANTMessageProtocol
-RUN swift build
